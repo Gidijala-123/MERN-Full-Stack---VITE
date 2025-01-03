08145a400c862e011a81d5889577edf3011c10ad
Bhargava Gidijala <85632729+Gidijala-123@users.noreply.github.com>
2025-01-03 09:26:41 +0530

    Created Routes

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/about";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 const App = () => {
-  return <h1 className="text-3xl text-green-600">Hello Bhargava..!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        {/* <Route path="*" element={<NotFound/>}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
